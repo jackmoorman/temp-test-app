a6ffc6a5ad4d2c47674c101c92263d5b85162d64
Jack Moorman <jackmoorman@Jacks-Work-MBP.local>
2023-06-21 16:44:44 -0400

    Added bin/cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,32 @@
 #!/usr/bin/env node
 
-// const repoName = process.argv[2];
-// const gitCheckoutCommand
+const { execSync } = require('child_process')
+
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, {stdio: 'inherit'})
+  } catch (e) {
+    console.error(`Failed to execute ${command}`, e);
+    return false;
+  }
+
+  return true;
+}
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth https://github.com/jackmoorman/temp-test-app ${repoName}`
+const installDepsCommand = `cd ${repoName} && npm install`
+
+console.log(`Cloning the repository with name ${repoName}`)
+
+const checkedOut = runCommand(gitCheckoutCommand)
+
+if (!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+
+const installedDeps = runCommand(installDepsCommand);
+if (!installedDeps) process.exit(-1);
+
+console.log('Congratulations! You are ready. Follow the following commands to start.')
+console.log(`cd ${repoName} && npm run dev`);
